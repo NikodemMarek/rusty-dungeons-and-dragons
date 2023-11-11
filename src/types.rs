0e859f0bb76149a041164aaeb6d2886c12462e09
git.nix <nikodemmarek11@gmail.com>
2023-11-11 15:52:21 +0000

    feat: room manages connections

--- src/types.rs
+++ src/types.rs
@@ -1,104 +1,48 @@
-use axum::extract::ws;
-use eyre::Result;
 use std::{collections::HashMap, sync::Arc};
 use tokio::sync::Mutex;
 
 use crate::game::Game;
 
 #[derive(Debug)]
-pub struct Client {
-    pub agent: String,
-    pub socket: ws::WebSocket,
-    // reciever: futures_util::stream::SplitStream<ws::WebSocket>,
-    // sender: futures_util::stream::SplitSink<ws::WebSocket, ws::Message>,
-}
-impl Client {
-    pub fn new(socket: ws::WebSocket, agent: String) -> Self {
-        use futures_util::StreamExt;
-
-        // let (sender, reciever) = socket.split();
-        Self {
-            agent,
-            socket, // reciever,
-                    // sender,
-        }
-    }
-
-    // pub async fn send(&mut self, msg: ws::Message) -> Result<()> {
-    //     use futures_util::SinkExt;
-    //     Ok(self.sender.send(msg).await?)
-    // }
-    // pub async fn next(&mut self) -> Result<ws::Message> {
-    //     use futures_util::StreamExt;
-    //     match self.reciever.next().await {
-    //         Some(Ok(msg)) => Ok(msg),
-    //         Some(Err(e)) => Err(e.into()),
-    //         None => Err(eyre::eyre!("no message")),
-    //     }
-    // }
-}
-
-#[derive(Debug)]
 pub struct Room {
     pub name: String,
+    pub game: Arc<Mutex<Game>>,
 
-    pub clients: std::collections::HashMap<usize, Client>,
     pub tx: tokio::sync::broadcast::Sender<String>,
-    rx: tokio::sync::broadcast::Receiver<String>,
-    pub game: Game,
+    recv_task: tokio::task::JoinHandle<()>,
 }
 impl Room {
     pub fn new(name: &str) -> Self {
-        let broadcast = tokio::sync::broadcast::channel(10);
+        let game = Arc::new(Mutex::new(Game::new()));
+        let (tx, mut rx) = tokio::sync::broadcast::channel::<String>(10);
+
+        let recv_game = game.clone();
+        let recv_task = tokio::spawn(async move {
+            while let Ok(msg) = rx.recv().await {
+                // TODO: Somehow get the sender from the message
+                let mut game = recv_game.lock().await;
+                game.push_player_msg(0, msg.clone());
+
+                println!("sbd said: {msg}");
+            }
+        });
 
         Self {
             name: name.to_owned(),
-            clients: std::collections::HashMap::new(),
-            tx: broadcast.0,
-            rx: broadcast.1,
-            game: Game::new(),
+            game: Arc::new(Mutex::new(Game::new())),
+            tx,
+            recv_task,
         }
     }
 
-    pub async fn add_client(&mut self, socket: ws::WebSocket, client: &str) {
-        let id = self.game.add_player(client.to_owned());
-        // self.clients.insert(id, client);
-
-        // if let Some(client) = self.clients.get_mut(&id) {
-        use futures_util::{SinkExt, StreamExt};
-        let (mut sender, mut reciever) = socket.split();
-
-        println!("atnehu");
-
-        let mut rx = self.tx.subscribe();
-        let mut send_task = tokio::spawn(async move {
-            while let Ok(msg) = rx.recv().await {
-                println!("{msg}");
-                if sender.send(ws::Message::Text(msg)).await.is_err() {
-                    break;
-                }
-            }
-        });
-
-        println!("aaaaaa");
-
-        let tx = self.tx.clone();
-        let mut recv_task = tokio::spawn(async move {
-            while let Some(Ok(ws::Message::Text(text))) = reciever.next().await {
-                println!("{text}");
-                let _ = tx.send(format!(": {text}"));
-            }
-        });
-
-        println!("duuuuuu");
-
-        tokio::select! {
-            _ = (&mut send_task) => recv_task.abort(),
-            _ = (&mut recv_task) => send_task.abort(),
-        };
-
-        println!("Websocket context destroyed");
-        // }
+    pub async fn add_client(&self, agent: &str) -> usize {
+        self.game.lock().await.add_player(agent.to_owned())
+    }
+}
+/// Make sure the reciever task is ended when the room is dropped
+impl Drop for Room {
+    fn drop(&mut self) {
+        self.recv_task.abort();
     }
 }
 
